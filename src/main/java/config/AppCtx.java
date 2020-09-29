c2f4cea9f0c3b05dd3885402fd6f58f5ad237c68
hjkim <pjt911219@gmail.com>
2020-09-29 11:35:14 +0900

    commit

---
+++ src/main/java/config/AppCtx.java
@@ -0,0 +1,68 @@
+package config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+import spring.MemberPrinter;
+import spring.MemberSummaryPrinter;
+import spring.VersionPrinter;
+
+@Configuration
+//@ComponentScan(basePackages = {"spring"},
+//				excludeFilters=@Filter(type=FilterType.REGEX, pattern="spring\\..*Dao"))
+
+@ComponentScan(basePackages = {"spring"},
+excludeFilters=@Filter(type=FilterType.ASPECTJ, pattern="spring.*Dao"))
+
+public class AppCtx {
+
+//	@Bean
+//	public MemberDao memberDao() {
+//		return new MemberDao();
+//	}
+	
+//	@Bean
+//	public MemberRegisterService memberRegSvc() {
+//		return new MemberRegisterService();
+//	}
+	
+//	@Bean
+//	public ChangePasswordService changePwdSvc() {
+//		return new ChangePasswordService();
+//	}
+	
+	@Bean
+	@Qualifier("printer")
+	public MemberPrinter memberPrinter1() {
+		return new MemberPrinter();
+	}
+	
+	@Bean
+	@Qualifier("summaryPrinter")
+	public MemberSummaryPrinter memberPrinter2() {
+		return new MemberSummaryPrinter();
+	}
+//	
+//	@Bean
+//	public MemberListPrinter listPrinter() {
+//		return new MemberListPrinter();
+//	}
+//	
+//	@Bean
+//	public MemberInfoPrinter infoPrinter() {
+//		MemberInfoPrinter infoPrinter = new MemberInfoPrinter();
+//		return infoPrinter;
+//	}
+	
+	@Bean
+	public VersionPrinter versionPrinter() {
+		VersionPrinter versionPrinter = new VersionPrinter();
+		versionPrinter.setMajorVersion(5);
+		versionPrinter.setMinorVersion(0);
+		return versionPrinter;
+	}
+}
